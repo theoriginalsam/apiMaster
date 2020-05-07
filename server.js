27c3ad9762ec7d22cbc0c346350dd5c83a0a8e7f
Samir Poudel <user@users-MacBook-Pro.local>
2020-05-07 20:00:04 +0545

    commit to the changes

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ dotenv.config({path:'./config/config.env'})
 const app = express()
 
 
+app.get('/',(req,res)=>{
+     res.json({greet :"Hellos"})
+})
 
-const PORT = process.env.PORT || 6000
-const env = process.env.NODE_ENV  
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 7000
+const env = process.env.NODE_ENV
 
 app.listen(PORT,console.log(`Server running on ${PORT} the API is in ${env} mode`)) 
